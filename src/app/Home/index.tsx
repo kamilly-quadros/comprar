eceb147e183a6fef40f75f68b510c693b9f9f6d0
K Vitória Quadros <vitoria.quadros@sertao.com.br>
2025-10-24 12:44:32 -0400

    Path Alias

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,4 @@
+import { styles } from "./styles";
+import { View, Image } from "react-native"
+
+export function Home() { return (<View style={styles.container}><Image source={require("@/assets/logo.png")} style={styles.logo} /></View>) }
